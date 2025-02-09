8e6da6c52991f740446c18237ccba75dc6f84dc4
Wai Chen <waichen2004@gmail.com>
2025-02-09 17:59:13 -0500

    fixed making creating account bug

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/users'
 
-const user = async (userObject)  => {
+const signup = async (userObject)  => {
   const response = await axios.post(baseUrl, userObject)
   return response.data
 }
 
-export default { user }
+export default { signup }
